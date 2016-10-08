20912f6c1a11701adad912204dff84621e9d50d1
phungtruong <phungtruong103@gmail.com>
2016-10-08 18:41:55 +0700

    Phung truong da up version 2

---
+++ app.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+var socketio = require('socket.io')(server);
+//set up server
+server.listen(process.env.PORT || 7777);
+//get the request
+app.get('/', function(req,res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+//socket io listen a connection event
+socketio.on('connection',function(socket){
+	console.log('Co nguoi ket noi');
+	socket.on('client-gui-tin-nhan',function(message){
+		socketio.emit('server-gui-tin-nhan',{"username": socket.un, "message":message});
+	});
+	socket.on('client-gui-username',function(username){
+		socket.un = username;
+	});
+});
