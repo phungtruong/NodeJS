60e73e2ec28edc644b6457d9987773b44ba8e51e
phungtruong <phungtruong103@gmail.com>
2017-02-03 22:40:13 +0700

    fsdfsdf

--- app.js
+++ app.js
@@ -452,11 +452,9 @@ socketio.on('connection',function(socket)
 			var message = mangdacat[4];
 			updateNoneStatusRoom(IDRoom, function(result){
 				insertMessage(IDRoom, IDuser, message, function(result, id){
-					getMessage(id, function(msg){
-						var time = msg.Time;
+						var time = new Date();
 						socketio.to(IDRoom+'').emit('server-gui-message',{IDRoom : IDRoom, IDuser : IDuser, message: message, name : name, avatar : avatar, time:time});
 						socketio.to(IDRoom+'').emit('server-gui-message-in-room',{IDRoom : IDRoom, IDuser : IDuser, message: message, name : name, avatar : avatar, time:time});
-					});
 				});
 			});
 
